b1116b8c4b0904b1297fe25458fe7138f955d2ff
Thomas Poth <thomas.poth@pubt.de>
2023-08-04 08:47:00 +0200

    Geheimzahl generieren und ausgeben
    Ausgabe soll in der finalen Version nicht mehr enthalten sein.
    Diese wurde nur für die Entwicklungszeit eingebaut.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Rate die Zahl!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Die Geheimzahl ist: {secret_number}");
 
     println!("Bitte gib Deine Schätzung ein.");
 
